49c28808d90ccc4f8d2f1dd35d9268adfb04f9f3
Charles Machalow <csm10495@gmail.com>
2020-12-30 09:44:43 -0800

    modify setup.py, add license, readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-tbd
+# PyDeskband
+
+PyDeskband is a multi-part project with two interconnected pieces of code.
+
+## C++
+
+There is a C++ DLL that is loaded via regsvr32.exe (into Windows Explorer) to create and paint a Deskband on the Windows Taskbar.
+
+## Python
+
+The Python front-end contains a means to control the C++ backend to control what is painted onto the Deskband. After installing the dll, it's easy to manipulate the deskband:
+
+<img src="https://i.imgur.com/TJkWOhb.png">
+
+# What is a Deskband?
+
+A Deskband is an additonal toolbar placed on the right-hand-side of the Windows Taskbar. Interestingly, enough they are considered deprecated as of Windows 10, but as of this writing still work just fine. Here is some [documentation](https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/cc144099(v=vs.85)) that includes high-level information on Deskbands.
+
+## Any Other Deskband Examples?
+
+Another example of a deskband is [XMeters](https://entropy6.com/xmeters/)
+
+## License
+MIT License
--- setup.py
+++ setup.py
@@ -8,9 +8,12 @@ import shutil
 import subprocess
 import sys
 
+if os.name != 'nt':
+    raise EnvironmentError(f"This is only supported on Windows... not for: {os.name}")
+
 THIS_FOLDER = os.path.abspath(os.path.dirname(__file__))
 
-def getVersion():
+def getVersion() -> str:
     with open(os.path.join(THIS_FOLDER, 'pydeskband', '__init__.py'), 'r') as f:
         text = f.read()
 
@@ -21,7 +24,8 @@ def getVersion():
 
     raise EnvironmentError("Unable to find __version__!")
 
-def get_msbuild():
+def get_msbuild() -> str:
+    ''' globs to find VS 2019's instance of MSBuild.exe '''
     matches = glob.glob(r'C:\Program Files*\Microsoft Visual Studio\2019\*\MSBuild\*\Bin\MSBuild.exe')
     if matches:
         print(f"MSBuild: {matches[0]}")
@@ -29,14 +33,16 @@ def get_msbuild():
 
     raise EnvironmentError("Could not find MSBuild for VS 2019!")
 
-def get_sln():
+def get_sln() -> pathlib.Path:
     sln = pathlib.Path(THIS_FOLDER) / "dll/PyDeskband/PyDeskband.sln"
     if not sln.is_file():
         raise FileNotFoundError(f"Could not find sln file: {sln}")
 
     return sln
 
-def run_msbuild(configuration, platform):
+def run_msbuild(configuration, platform) -> pathlib.Path:
+    ''' Runs MSBuild for the given configuration/platform. Returns the path to the built dll '''
+
     if configuration not in ('Debug', 'Release'):
         raise ValueError("configuration should be Debug or Release")
     if platform not in ('x64', 'x86'):
@@ -57,7 +63,9 @@ def run_msbuild(configuration, platform):
 class BuildPyCommand(build_py):
     """Custom build command. That will build dlls using MSBuild"""
     def build_and_copy_dlls(self):
-        # Build x64 and x86 versions of the dll
+        '''
+        Build x64 and x86 versions of the dll. Then copies them to pydeskband/dlls within the Python package
+        '''
         x64_dll = run_msbuild('Release', 'x64')
         x86_dll = run_msbuild('Release', 'x86')
 
@@ -72,6 +80,9 @@ class BuildPyCommand(build_py):
         print("DLLs have been copied!")
 
     def run(self):
+        '''
+        Called to perform the build_py step
+        '''
         self.build_and_copy_dlls()
         build_py.run(self)
 
@@ -83,7 +94,7 @@ setup(
     version=getVersion(),
     packages=['pydeskband'],
     license='MIT License',
-    python_requires='>=3,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,!=3.6.*',
+    python_requires='>=3.7',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     long_description_content_type="text/markdown",
     classifiers=[
